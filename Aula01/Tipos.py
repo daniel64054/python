bcb8414dc75a5ca24f4e1870160dbcb1cced24a8
Daniel Mendes <dm099150@gmail.com>
2023-11-27 09:20:22 -0300

    criação do arquivo  Tipos.py com tipos de variaveis

---
+++ Aula01/Tipos.py
@@ -0,0 +1,11 @@
+#comentario de uma linha
+'''
+este é um comentario de varias linha
+'''
+
+#vamos criar variaveis e ver seus tipos
+nome = "Rafa"
+idade = 312
+preca = 0.5
+cidades = ["campinas","leme", "vasco"]
+temfilho = False
