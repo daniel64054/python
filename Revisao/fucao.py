a6b94b378452a6ef76433b10fe3c1fabd29c6bff
daniel <dm099150@gmail.com>
2024-01-18 11:52:06 -0300

    arquivos para revição com fução

---
+++ Revisao/fucao.py
@@ -0,0 +1,15 @@
+"""
+este programa possui uma função que diz quanta letras tem um texto
+
+para criar fução é necessario usar def
+"""
+def qtletras(texto):
+    '''
+    a função recebe um texto e exibe a quantidade de letras presentes(espaços contam)
+    '''
+    print(len(texto))
+
+frase  = "hoje é quinta-feira DIA DE FEIJOADA"
+
+
+qtletras(frase)
---
+++ Revisao/funcao3.py
@@ -0,0 +1,8 @@
+#import funcao2 as fun
+from  funcao2 import  soma as sm
+
+n =[12,31,6,47,75,87,67,8]
+
+print(sm(n))
+print(fun.maior(n))
+
